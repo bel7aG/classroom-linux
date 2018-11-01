c0905b749f0ee5b17a8ec50a22c01f07996e6883
bel7aG <belhassen.gharsallah@isetrades.com>
2018-11-01 03:25:01 +0100

    bad things :/

---
+++ basics/1-step.sh
@@ -0,0 +1,23 @@
+mkdir f_1-step
+cd f_1-step
+
+                        # work for Exercice 1
+
+touch annee1 Annee2 annee4 annee41 annee45 annee510 annee_saucisse bananne
+
+mkdir Year && mv [aAb]* Year
+
+ls *5
+ls annee4*
+ls | grep "^annee4"
+ls | grep "^annee[^0-9]"
+ls | egrep "^[0-9]+$"
+ls *ana*
+ls | egrep "^[Aa]"
+mv "*[41]{1}$" /tmp
+
+                      # work for Exercice Three
+cd ..
+mkdir f_2-step
+cd f_2-step
+                      
