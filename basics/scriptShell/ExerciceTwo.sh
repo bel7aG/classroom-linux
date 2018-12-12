31a4d16f44f518c44c8528112de25fcca2fec501
Belhassen Gharsallah <belhassengharsallah@gmail.com>
2018-12-12 20:27:14 +0100

    zsh test commit

--- basics/scriptShell/ExerciceTwo.sh
+++ basics/scriptShell/ExerciceTwo.sh
@@ -1,9 +1,41 @@
-echo -e "$\n\n\n\t*\t*\t*\t*\tMenu general\t*\t*\t*\t*\t\n\n"
-echo -e "\t\t\n\n\n\n<1>`date`"
-echo -e "\t\t\n\n<2>` ps | wc -l`"
-
-for i in /etc/passwd
+isOkey="LOL"
+while [ $isOkey ]
 do
+  echo -e "$\n\n\n\t*\t*\t*\t*\tMenu general\t*\t*\t*\t*\t\n\n"
+  echo -e "\t\t\t\t\t\t\n\n\n\n<1> Afficher la date"
+  echo -e "\t\t\t\t\t\t\n\n\n\n<2> le nombre de processus dans le system"
+  echo -e "\t\t\t\t\t\t\n\n\n\n<3> Afficher tous les user ayant uid = 10"
+  echo -e "\t\t\t\t\t\t\n\n\n\n<4> Good Buy"
+  echo -e "$\n\n\n\t*\t*\t*\t*\t-----------------\t*\t*\t*\t*\t\n\n"
+  echo -e "\t\t\t\t*************************************************"
+  echo "Your Choice: "
+  read choice
+  case $choice in
+    1)
+      date
+    ;;
 
+    2)
+      ps -x | wc -l
+    ;;
 
-echo -e "\t\t\t\t\n\n<3>`cut -d 3 /etc/passwd`"
+    3)
+      cat /etc/passwd | cut -d ':' -f3 | grep ':51'
+      cat /etc/passwd | cut -d ':' -f3 | grep ':52'
+      cat /etc/passwd | cut -d ':' -f3 | grep ':53'
+      cat /etc/passwd | cut -d ':' -f3 | grep ':53'
+      cat /etc/passwd | cut -d ':' -f3 | grep ':54'
+      cat /etc/passwd | cut -d ':' -f3 | grep ':55'
+      cat /etc/passwd | cut -d ':' -f3 | grep ':56'
+      cat /etc/passwd | cut -d ':' -f3 | grep ':57'
+      cat /etc/passwd | cut -d ':' -f3 | grep ':58'
+      cat /etc/passwd | cut -d ':' -f3 | grep ':59'
+    ;;
+
+    4)
+      exit
+    ;;
+
+  esac
+  read x
+done
